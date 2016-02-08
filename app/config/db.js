09ed824f604bcc8ec6d381182deabed72a1b7eb5
SJTreadway <treadway08@gmail.com>
2016-02-08 12:03:49 -0600

    deletes everything

--- app/config/db.js
+++
@@ -1,8 +0,0 @@
-import config from './config.json';
-
-export default function (passport) {
-  const db = require('../' + config[config.db].init);
-  const passportConfig = require('../' + config[config.db].passport);
-  passportConfig(passport, db);
-  return db;
-}
--- run.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-/* A wrapper to transpile all ES6 to ES5
-  This can be removed when node starts to fully support ES6
- */
-
-require('babel/register');
-require('./app/config/server');
